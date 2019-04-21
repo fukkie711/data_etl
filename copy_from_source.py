7a9e2267f5d5f5b755354deab79677bd8e5aee8c
fukkie0711 <fukuzawa0711@gmail.com>
2019-04-21 09:48:00 +0900

    modify and del

--- copy_from_source.py
+++ copy_from_source.py
@@ -2,19 +2,28 @@
 #coding:utf-8
 import shutil
 import os
+import codecs
 from pathlib import Path
 ipt = ""
 opt = ""
 
-ipt = "/Users/k-fukuzawa/Documents/bin/JPG_2014039/DOCUMENT/A/2014132001/2014132801"
-opt = "/Users/k-fukuzawa/Documents/bin/output"
+ipt = "/Users/k-fukuzawa/Documents/bin/JPG_2014039/DOCUMENT/A/2014132001/2014132801/"
+opt = "/Users/k-fukuzawa/Documents/bin/output/"
 
 p = Path(ipt)
 xml_path = p.glob('**/*.xml')
 pdf_path = p.glob('**/*.pdf')
 
-def ipt_copy_to_opt():
+def chg_ipt_codec():
     for i in xml_path:
-        print(i)
-        shutil.copy2(i, opt)
-ipt_copy_to_opt()
+        with open(opt + i.name, 'w', encoding='utf_8') as fout:
+            with open(i, encoding='euc_jp') as fin:
+                fout.write(fin.read())
+chg_ipt_codec()
+
+# def ipt_copy_to_opt():
+#     for i in xml_path:
+#         print(i)
+#         shutil.copy2(i, opt)
+#
+# ipt_copy_to_opt()
--- one.py
+++
@@ -1,5 +0,0 @@
-# this is for importing data from DVDs.
-dat_from = ""
-dat_to = ""
-
-dat_from = "/Users/k-fukuzawa/Documents/bin"
