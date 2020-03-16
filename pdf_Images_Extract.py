8f344c3ce5e3ba9ef4fcb8176d57030c3ee4f871
k-fukuzawa <k-fukuzawa@aitech.ac.jp>
2020-03-16 12:41:46 +0900

    new file

---
+++ pdf_Images_Extract.py
@@ -0,0 +1,17 @@
+# pip install PyMuPDF
+import fitz
+import sys
+
+input = sys.argv
+doc = fitz.open(input[1])
+for i in range(len(doc)):
+    for img in doc.getPageImageList(i):
+        xref = img[0]
+        pix = fitz.Pixmap(doc, xref)
+        if pix.n < 5:       # this is GRAY or RGB
+            pix.writePNG("p%s-%s.png" % (i, xref))
+        else:               # CMYK: convert to RGB first
+            pix1 = fitz.Pixmap(fitz.csRGB, pix)
+            pix1.writePNG("p%s-%s.png" % (i, xref))
+            pix1 = None
+        pix = None
