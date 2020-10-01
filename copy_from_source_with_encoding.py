895f506ea069cc7c10e0c1addb6acec8afe941b3
fukkie711 <k-fukuzawa@aitech.ac.jp>
2020-10-01 16:40:26 +0900

    minor changes

--- copy_from_source_with_encoding.py
+++ copy_from_source_with_encoding.py
@@ -28,23 +28,31 @@ if len(sys.argv) < 3:
 ipt = sys.argv[1]
 opt = sys.argv[2]
 
-p = Path(ipt)
+ipt_path = Path(ipt)
+opt_path = Path(opt)
 
 def copy_xml_and_chg_ipt_codec():
-    xml_path = p.glob('**/*.xml')
+    xml_path = ipt_path.glob('**/*.xml')
     for i in xml_path:
-        with open(opt + i.name, 'w', encoding='utf_8') as f_out:
-            print(opt, i.name)
+        with open(opt_path + i.name, 'w', encoding='utf_8') as f_out:
+            print(opt_path, i.name)
             with open(i, encoding='euc_jp') as f_in:
                 # 下記はxmlからcsvに変換する際に、euc-jpの記載があると「マルチバイト文字列は使えません」みたいなエラーが出て進まないのでやむを得ず外科手術を行った結果がこれである…
                 f_out.write(f_in.read().replace('<?xml version="1.0" encoding="EUC-JP"?>', '<?xml version="1.0" encoding="UTF-8"?>'))
 def copy_pdf(): #copy pdf file
-    pdf_path = p.glob('**/*.pdf')
+    pdf_path = ipt_path.glob('**/*.pdf')
     for i in pdf_path:
-        shutil.copy2(i, opt + i.name)
+        print(opt_path,i.name )
+        shutil.copy2(i, opt_path + i.name)
 
 copy_xml_and_chg_ipt_codec()
+print("----------------------------------------")
+print("----------xml copy files done.----------")
+print("----------------------------------------")
 copy_pdf()
+print("----------------------------------------")
+print("----------pdf copy files done.----------")
+print("----------------------------------------")
 
 # def ipt_copy_to_opt():
 #     for i in xml_path:
