4b1969c323a6639ecc0a06d8fd565a5d0593b0ca
fukkie711 <fukuzawa0711@gmail.com>
2023-01-08 01:38:29 +0900

    modify: new_copy_xml_and_pdf

--- new_copy_xml_and_pdf.py
+++ new_copy_xml_and_pdf.py
@@ -1,9 +1,6 @@
-# this is for importing xml and pdf files from sources and change xml file encodings from euc-jp to utf-8.
 #coding:utf-8
-
 import shutil
 import os
-import codecs
 import sys
 import datetime
 import pathlib
@@ -11,19 +8,8 @@ import time
 import glob
 from tqdm import tqdm
 
-
-# from pathlib import Path
-
-ipt = ""
-opt = ""
-
-# テスト用ディレクトリ
-# ipt = "/Users/k-fukuzawa/Dropbox/tmp/01input/"
-# opt = "/Users/k-fukuzawa/Dropbox/tmp/01output/"
-
 # コマンドライン引数を使ってインプットディレクトリとアウトプットディレクトリを指定する
-# 1はインプット，2はアウトプットディアレク取りを指定する
-
+# 1はインプット，2はアウトプットディレクトリを指定する
 if len(sys.argv) < 2:
       print("インプットディレクトリを入力してください")
       sys.exit()
@@ -31,6 +17,7 @@ if len(sys.argv) < 3:
       print("アウトプットディレクトリを入力してください")
       sys.exit()
 
+# モードによって抽出するファイルの種類をかえる。同時にやることもできる。
 xml_mode = False
 pdf_mode = False
 csv_extract_mode = False
@@ -39,13 +26,14 @@ for arg in sys.argv:
         xml_mode = True
     if arg == '--pdf':
         pdf_mode = True
-    if arg == 'csv_extract':
-        csv_extract_mode = True
+    # if arg == '--csv_extract':
+        # csv_extract_mode = True
 
 input_dir = pathlib.Path(sys.argv[1])
 output_dir = pathlib.Path(sys.argv[2])
 
 def extract_xml(input_dir, output_dir):
+    start_time = datetime.datetime.now()
     files = os.listdir(input_dir)
     for dir in tqdm(files):
         # if os.path.isdir(dir):
@@ -62,10 +50,11 @@ def extract_xml(input_dir, output_dir):
         print(f'number of xml_files: {len(xml_files)}')
         for xml_file in tqdm(xml_files):
             shutil.copy2(src=xml_file, dst=output_dir/dir/xml_directory)
-    now = datetime.datetime.now()
-    print("----------------------------------------")
-    print("{0}: {1:%Y/%m/%d %H:%M} .xml copy files done.".format(output_dir, now))
-    print("----------------------------------------")
+    finish_time = datetime.datetime.now()
+    print("--------------------------------------------------------------------")
+    print("{0}: {1:%Y/%m/%d %H:%M} .pdf copy files start.".format(output_dir, start_time))
+    print("{0}: {1:%Y/%m/%d %H:%M} .pdf copy files done.".format(output_dir, finish_time))
+    print("--------------------------------------------------------------------")
 
 def extract_pdf(input_dir, output_dir):
     start_time = datetime.datetime.now()
@@ -84,10 +73,25 @@ def extract_pdf(input_dir, output_dir):
             shutil.copy2(src=pdf_file, dst=output_dir/dir/pdf_directory)
     finish_time = datetime.datetime.now()
     print("--------------------------------------------------------------------")
-    print("{0}: {1:%Y/%m/%d %H:%M} .pdf copy files start.".format(output_dir, finish_time))
+    print("{0}: {1:%Y/%m/%d %H:%M} .pdf copy files start.".format(output_dir, start_time))
     print("{0}: {1:%Y/%m/%d %H:%M} .pdf copy files done.".format(output_dir, finish_time))
     print("--------------------------------------------------------------------")
 
+# def csv_extract(input_dir, output_dir):
+#     start_time = datetime.datetime.now()
+
+#     files = os.listdir(input_dir)
+#     for dir in tqdm(files):
+#         xml_files = glob.glob(f'{input_dir/dir}/**/*.xml', recursive=True)
+#         print(input_dir/dir)
+
+
+#     finish_time = datetime.datetime.now()
+#     print("--------------------------------------------------------------------")
+#     print("{0}: {1:%Y/%m/%d %H:%M} csv_extract start.".format(output_dir, start_time))
+#     print("{0}: {1:%Y/%m/%d %H:%M} csv_extract done.".format(output_dir, finish_time))
+#     print("--------------------------------------------------------------------")
+    
 if xml_mode and pdf_mode:
     print('extract .xml and .pdf')
     extract_xml(input_dir, output_dir)
@@ -98,7 +102,8 @@ elif xml_mode:
 elif pdf_mode:
     print('extract .pdf only')
     extract_pdf(input_dir, output_dir)
-elif csv_extract_mode:
-    print('csv_extract_mode')
+# elif csv_extract_mode:
+#     print('csv_extract_mode')
 else:
-    print('[USAGE] copy_xml_and_pdf.py src dst [--xml][--pdf][--csv_extract ]')
+    # print('[USAGE] copy_xml_and_pdf.py src dst [--xml][--pdf][--csv_extract ]')
+    print('[USAGE] copy_xml_and_pdf.py src dst [--xml][--pdf]')
